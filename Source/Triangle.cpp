0b4ca55d1f78c8d2125f6da3a92ba531277a015a
maxnevans <maxnevans@gmail.com>
2019-03-17 21:47:49 +0300

    Triangle drawing bag fixed

--- Source/Triangle.cpp
+++ Source/Triangle.cpp
@@ -11,7 +11,7 @@ void Custom::Triangle::Redraw(Gdiplus::Graphics* const graphics)
 	Point points[3];
 	points[0] = Point(this->x1, this->y2);
 	points[1] = Point(this->x2, this->y2);
-	points[2] = Point(this->x1 + this->width / 2, this->y1);
+	points[2] = Point(this->x + this->width / 2, this->y1);
 
 	graphics->DrawPolygon(&pen, points, 3);
 }
