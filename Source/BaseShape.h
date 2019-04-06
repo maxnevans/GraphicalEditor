6a3dddaa4595bab02b01ac710ff616084091837f
maxnevans <maxnevans@gmail.com>
2019-04-06 20:20:20 +0300

    BaseShape SetPointsSafe method implemented, interfaces added ICursor and IDrawable

--- Source/BaseShape.h
+++ Source/BaseShape.h
@@ -3,19 +3,27 @@
 #include <gdiplus.h>
 #include "ISerializable.h"
 #include "IDeserializable.h"
+#include "ICursor.h"
+#include "IDrawable.h"
 
 namespace Custom {
 	class BaseShape :
 		public ISerializable,
-		public IDeserializable
+		public IDeserializable,
+		public ICursor,
+		public IDrawable
 	{
 	public:
 		BaseShape(const wchar_t* name);
-		virtual void Redraw(Gdiplus::Graphics* const graphics) = 0;
+		virtual void SelectPoint(int x, int y) override;
+		virtual void SelectArea(int x1, int y1, int x2, int y2) override;
 		virtual void SetPoints(int x1, int y1, int x2, int y2);
 		void SetColor(Gdiplus::Color color);
 		virtual std::wstring SerializeText() override;
 		virtual void DeserializeText(std::wstring text) override;
+
+	protected:
+		void SetPointsSafe(int x1, int y1, int x2, int y2);
 
 	protected:
 		int x, y, width, height;
