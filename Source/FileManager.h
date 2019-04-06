8bae8440d0cad0a643d3c4848c73008f5391640b
maxnevans <maxnevans@gmail.com>
2019-04-06 18:29:54 +0300

    FileManager SaveText and LoadText fixed

--- Source/FileManager.h
+++ Source/FileManager.h
@@ -1,11 +1,10 @@
 #pragma once
-#include "ISerializable.h"
-#include "IDeserializable.h"
+#include "ListShapes.h"
 
 class FileManager
 {
 public:
-	static void SaveText(ISerializable* object, std::wstring filename);
-	static void LoadText(IDeserializable* object, std::wstring filename);
+	static void SaveText(ListShapes* shapes, std::wstring filename);
+	static void LoadText(ListShapes* shapes, std::wstring filename);
 };
 
