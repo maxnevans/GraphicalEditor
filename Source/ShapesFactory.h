669df7c7ccf2d7050df28da792262a69733d30c9
maxnevans <maxnevans@gmail.com>
2019-04-06 18:31:07 +0300

    ShapesFactory implemented

---
+++ Source/ShapesFactory.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+#include "BaseShape.h"
+
+class ShapesFactory
+{
+public:
+	static Custom::BaseShape* CreateShape(std::wstring name);
+
+public:
+	static constexpr const wchar_t* LINE		= L"Line";
+	static constexpr const wchar_t* RECTANGLE	= L"Rectangle";
+	static constexpr const wchar_t* SQUARE		= L"Square";
+	static constexpr const wchar_t* TRIANGLE	= L"Triangle";
+	static constexpr const wchar_t* POLYGON		= L"Polygon";
+	static constexpr const wchar_t* ELLIPSE		= L"Ellipse";
+	static constexpr const wchar_t* CIRCLE		= L"Circle";
+};
+
