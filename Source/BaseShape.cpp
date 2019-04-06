b66174cc921536ffb5d11aaae39e2dc2ea09c384
maxnevans <maxnevans@gmail.com>
2019-04-06 18:32:18 +0300

    BaseShape serialization/deserialization implemented

--- Source/BaseShape.cpp
+++ Source/BaseShape.cpp
@@ -1,11 +1,14 @@
 #include "BaseShape.h"
+#include "Exception.h"
+#include <sstream>
+#include <string>
 
 void Custom::BaseShape::SetPoints(int x1, int y1, int x2, int y2)
 {
-	this->x = (x1 - x2) < 0 ? x1 : x2;
-	this->y = (y1 - y2) < 0 ? y1 : y2;
-	this->width = (x1 - x2) < 0 ? x2 - x1 : x1 - x2;
-	this->height = (y1 - y2) < 0 ? y2 - y1 : y1 - y2;
+	this->x = x1 < x2 ? x1 : x2;
+	this->y = y1 < y2 ? y1 : y2;
+	this->width = x1 < x2 ? x2 - x1 : x1 - x2;
+	this->height = y1 < y2 ? y2 - y1 : y1 - y2;
 	this->x1 = x1;
 	this->y1 = y1;
 	this->x2 = x2;
@@ -15,4 +18,37 @@ void Custom::BaseShape::SetPoints(int x1, int y1, int x2, int y2)
 void Custom::BaseShape::SetColor(Gdiplus::Color color)
 {
 	this->color = color;
+}
+
+std::wstring Custom::BaseShape::SerializeText()
+{
+	std::wstringstream ss;
+	ss << "Coordinates: (" << this->x1 << ", " << this->y1 << ") (" << this->x2 << ", " << this->y2 << ") Color: " << this->color.GetValue();
+	return ss.str();
+}
+
+void Custom::BaseShape::DeserializeText(std::wstring text)
+{
+	std::wstringstream ss(text);
+
+	ss.ignore(wcslen(L"Coordinates: ("));
+	ss >> this->x1;
+	ss.ignore(2);
+	ss >> this->y1;
+	ss.ignore(wcslen(L") ("));
+	ss >> this->x2;
+	ss.ignore(2);
+	ss >> this->y2;
+	ss.ignore(wcslen(L") Color: "));
+
+	Gdiplus::ARGB colorValue;
+	ss >> colorValue;
+	this->color.SetValue(colorValue);
+
+	if (ss.fail()) throw Exception(L"undefined text format");
+
+	this->x = this->x1 < this->x2 ? this->x1 : this->x2;
+	this->y = this->y1 < this->y2 ? this->y1 : this->y2;
+	this->width = this->x1 < this->x2 ? this->x2 - this->x1 : this->x1 - this->x2;
+	this->height = this->y1 < this->y2 ? this->y2 - this->y1 : this->y1 - this->y2;
 }
--- Source/BaseShape.h
+++ Source/BaseShape.h
@@ -1,14 +1,20 @@
 #pragma once
 #include <windows.h>
 #include <gdiplus.h>
+#include "ISerializable.h"
+#include "IDeserializable.h"
 
 namespace Custom {
-	class BaseShape
+	class BaseShape :
+		public ISerializable,
+		public IDeserializable
 	{
 	public:
 		virtual void Redraw(Gdiplus::Graphics* const graphics) = 0;
 		virtual void SetPoints(int x1, int y1, int x2, int y2);
 		void SetColor(Gdiplus::Color color);
+		virtual std::wstring SerializeText() override;
+		virtual void DeserializeText(std::wstring text) override;
 
 	protected:
 		int x, y, width, height;
