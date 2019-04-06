007a9d6a300ec35c24778867cef30b75581309af
maxnevans <maxnevans@gmail.com>
2019-04-06 18:28:59 +0300

    Exception DebugException macro added

--- Source/Exception.cpp
+++ Source/Exception.cpp
@@ -1,11 +1,27 @@
 #include "Exception.h"
+#include <sstream>
 
-Exception::Exception(std::wstring message)
+_Exception::_Exception(std::wstring message)
 {
 	this->message = message;
+	this->line = -1;
+	this->filename = L"";
 }
 
-std::wstring Exception::what() noexcept
+_Exception::_Exception(std::wstring message, int line, std::wstring filename)
 {
-	return this->message;
+	this->message = message;
+	this->line = line;
+	this->filename = filename;
+}
+
+std::wstring _Exception::what() noexcept
+{
+	std::wstringstream ss;
+	ss << this->message;
+	if (this->line != -1)
+	{
+		ss << L" on line " << this->line << L" in file " << this->filename;
+	}
+	return ss.str();
 }
--- Source/Exception.h
+++ Source/Exception.h
@@ -1,13 +1,20 @@
 #pragma once
 #include <string>
+#define WIDE2(x) L##x
+#define WIDE1(x) WIDE2(x)
+#define DebugException(message) _Exception(message, __LINE__, WIDE1(__FILE__))
 
-class Exception
+class _Exception
 {
 public:
-	Exception(std::wstring message = L"");
+	_Exception(std::wstring message = L"");
+	_Exception(std::wstring message, int line, std::wstring filename);
 	virtual std::wstring what() noexcept;
 private:
 
 	std::wstring message;
+	int line;
+	std::wstring filename;
 };
 
+typedef _Exception Exception;
