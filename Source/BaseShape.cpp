de9ddf6f6fe46ebfc48b4deefa67415c2a1413a4
maxnevans <maxnevans@gmail.com>
2019-04-06 20:20:20 +0300

    BaseShape SetPointsSafe method implemented, interfaces added ICursor and IDrawable

--- Source/BaseShape.cpp
+++ Source/BaseShape.cpp
@@ -9,16 +9,18 @@ Custom::BaseShape::BaseShape(const wchar_t* name)
 {
 }
 
+void Custom::BaseShape::SelectPoint(int x, int y)
+{
+}
+
+void Custom::BaseShape::SelectArea(int x1, int y1, int x2, int y2)
+{
+	this->SetPointsSafe(x1, y1, x2, y2);
+}
+
 void Custom::BaseShape::SetPoints(int x1, int y1, int x2, int y2)
 {
-	this->x = x1 < x2 ? x1 : x2;
-	this->y = y1 < y2 ? y1 : y2;
-	this->width = x1 < x2 ? x2 - x1 : x1 - x2;
-	this->height = y1 < y2 ? y2 - y1 : y1 - y2;
-	this->x1 = x1;
-	this->y1 = y1;
-	this->x2 = x2;
-	this->y2 = y2;
+	this->SetPointsSafe(x1, y1, x2, y2);
 }
 
 void Custom::BaseShape::SetColor(Gdiplus::Color color)
@@ -58,4 +60,16 @@ void Custom::BaseShape::DeserializeText(std::wstring text)
 	this->y = this->y1 < this->y2 ? this->y1 : this->y2;
 	this->width = this->x1 < this->x2 ? this->x2 - this->x1 : this->x1 - this->x2;
 	this->height = this->y1 < this->y2 ? this->y2 - this->y1 : this->y1 - this->y2;
+}
+
+void Custom::BaseShape::SetPointsSafe(int x1, int y1, int x2, int y2)
+{
+	this->x = x1 < x2 ? x1 : x2;
+	this->y = y1 < y2 ? y1 : y2;
+	this->width = x1 < x2 ? x2 - x1 : x1 - x2;
+	this->height = y1 < y2 ? y2 - y1 : y1 - y2;
+	this->x1 = x1;
+	this->y1 = y1;
+	this->x2 = x2;
+	this->y2 = y2;
 }
