bb1df435d363e3f5f1cbeff357084c491b4f0ca8
maxnevans <maxnevans@gmail.com>
2019-04-06 18:42:56 +0300

    BaseShape name field added to determine which object it is

--- Source/BaseShape.h
+++ Source/BaseShape.h
@@ -10,6 +10,7 @@ namespace Custom {
 		public IDeserializable
 	{
 	public:
+		BaseShape(const wchar_t* name);
 		virtual void Redraw(Gdiplus::Graphics* const graphics) = 0;
 		virtual void SetPoints(int x1, int y1, int x2, int y2);
 		void SetColor(Gdiplus::Color color);
@@ -20,6 +21,7 @@ namespace Custom {
 		int x, y, width, height;
 		int x1, y1, x2, y2;
 		Gdiplus::Color color;
+		const wchar_t* name;
 	};
 }
 
