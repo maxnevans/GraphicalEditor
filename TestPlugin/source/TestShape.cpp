d6627bdb7f1f224c04275db5765ea5395d36d44a
maxnevans <maxnevans@gmail.com>
2019-05-17 22:20:21 +0300

    TestShape: added rectange around text

--- TestPlugin/source/TestShape.cpp
+++ TestPlugin/source/TestShape.cpp
@@ -6,12 +6,19 @@ void TestShape::Redraw(Gdiplus::Graphics* const graphics)
 	using namespace Gdiplus;
 	std::wstring test = L"Test Shape";
 	Gdiplus::FontFamily fontFamily(L"Arial");
-	Gdiplus::Font font(&fontFamily, 16, FontStyleRegular, UnitPixel);
-	PointF origin;
-	origin.X = static_cast<float>(this->x);
-	origin.Y = static_cast<float>(this->y);
-	Gdiplus::SolidBrush brush(Gdiplus::Color::Red);
-	graphics->DrawString(test.c_str(), -1,  &font, origin, &brush);
+	Gdiplus::Font font(&fontFamily, 40, FontStyleRegular, UnitPixel);
+	RectF layout;
+	layout.X = static_cast<float>(this->x);
+	layout.Y = static_cast<float>(this->y);
+	layout.Width = static_cast<float>(this->width);
+	layout.Height = static_cast<float>(this->height);
+	Color color(this->color);
+	SolidBrush brush(color);
+	Pen pen(color);
+	StringFormat sf;
+	sf.SetAlignment(StringAlignment::StringAlignmentCenter);
+	graphics->DrawRectangle(&pen, layout);
+	graphics->DrawString(test.c_str(), -1, &font, layout, &sf, &brush);
 }
 
 ::BaseShape* TestShape::ShapeFactory()
