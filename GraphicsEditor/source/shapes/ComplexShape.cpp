8637051038a160c98fef34575abe7021ec8d26e9
maxnevans <maxnevans@gmail.com>
2019-05-22 13:09:41 +0300

    ComplexShape: huge amount of bugs fixed

--- GraphicsEditor/source/shapes/ComplexShape.cpp
+++ GraphicsEditor/source/shapes/ComplexShape.cpp
@@ -1,32 +1,67 @@
 #include "ComplexShape.h"
+#include "../../../Exceptions/source/Exceptions.h"
+#include <assert.h>
+#include <string>
 
-ComplexShape::ComplexShape(std::vector<BaseShape*> shapes)
+ComplexShape::ComplexShape(const std::wstring& name, const std::vector<const BaseShape*>& shapes)
+	:
+	name(name),
+	shapes(shapes)
 {
-	this->shapes = shapes;
+	assert(shapes.size() > 0);
+
+	this->refWidth = shapes.front()->GetX() + shapes.front()->GetWidth();
+	this->refHeight = shapes.front()->GetY() + shapes.front()->GetHeight();
+
+	for (const BaseShape* shape : this->shapes)
+	{
+		this->gfxShapes.push_back(shape->Clone());
+		int w = shape->GetX() + shape->GetWidth();
+		int h = shape->GetY() + shape->GetHeight();
+		this->refWidth = this->refWidth < w ? w : this->refWidth;
+		this->refHeight = this->refHeight < h ? h : this->refHeight;
+	}
+		
+}
+
+ComplexShape::~ComplexShape()
+{
+	for (BaseShape* shape : this->gfxShapes)
+		delete shape;
 }
 
 void ComplexShape::Redraw(Gdiplus::Graphics* const graphics)
 {
-	for (BaseShape* shape : this->shapes)
+	for (BaseShape* shape : this->gfxShapes)
 		shape->Redraw(graphics);
 }
 
-const wchar_t* ComplexShape::GetName()
+void ComplexShape::SetPoints(int x1, int y1, int x2, int y2)
 {
-	return this->NAME;
+	BaseShape::SetPointsSafe(x1, y1, x2, y2);
+
+	float distortionX = static_cast<float>(this->width) / this->refWidth;
+	float distortionY = static_cast<float>(this->height) / this->refHeight;
+
+	for (size_t i = 0; i < this->shapes.size(); i++)
+	{
+		
+		float shapeX = this->shapes[i]->GetX();
+		float shapeY = this->shapes[i]->GetY();
+		float shapeWidth = this->shapes[i]->GetWidth();
+		float shapeHeight = this->shapes[i]->GetHeight();
+
+		shapeX = shapeX * distortionX + this->x;
+		shapeY = shapeY * distortionY + this->y;
+		shapeWidth *= distortionX;
+		shapeHeight *= distortionY;
+
+		this->gfxShapes[i]->SetPoints(shapeX, shapeY, shapeX + shapeWidth, shapeY + shapeHeight);
+	}
 }
 
-IShapeFactoryFunctor* ComplexShape::CreateFactoryFunctor(std::vector<BaseShape*> shapes)
+ComplexShape::ComplexShapeFactoryFunctor::~ComplexShapeFactoryFunctor()
 {
-	return new ComplexShapeFactoryFunctor(shapes);
-}
-
-ComplexShape::ComplexShapeFactoryFunctor::ComplexShapeFactoryFunctor(std::vector<BaseShape*> shapes)
-{
-	this->shapes = shapes;
-}
-
-BaseShape* ComplexShape::ComplexShapeFactoryFunctor::operator()()
-{
-	return new ComplexShape(this->shapes);
+	for (const BaseShape* shape : this->shapes)
+		delete shape;
 }
--- GraphicsEditor/source/shapes/ComplexShape.h
+++ GraphicsEditor/source/shapes/ComplexShape.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "../../../PluginSupporter/source/PluginSupporter.h"
+#include "../../../Exceptions/source/Exceptions.h"
 #include <vector>
+#include <string>
 
 class ComplexShape :
 	public BaseShape
@@ -11,23 +13,50 @@ private:
 		public IShapeFactoryFunctor
 	{
 	public:
-		ComplexShapeFactoryFunctor(std::vector<BaseShape*> shapes);
-		virtual BaseShape* operator()() override;
+		inline ComplexShapeFactoryFunctor(std::wstring name, std::vector<const BaseShape*>& shapes)
+			:
+			name(name),
+			shapes(shapes)
+		{
+		}
+		inline virtual BaseShape* operator()() override
+		{
+			return new ComplexShape(this->name, this->shapes);
+		}
+		virtual ~ComplexShapeFactoryFunctor() override;
 
 	private:
-		std::vector<BaseShape*> shapes;
+		const std::vector<const BaseShape*> shapes;
+		const std::wstring name;
 	};
 
 public:
-	ComplexShape(std::vector<BaseShape*> shapes);
+	ComplexShape(const std::wstring& name, const std::vector<const BaseShape*>& shapes);
+	virtual ~ComplexShape() override;
+	inline virtual BaseShape* Clone() const override
+	{
+		throw Exception(L"Clone is not implemented");
+		return nullptr;
+	}
 	virtual void Redraw(Gdiplus::Graphics* const graphics);
-	virtual const wchar_t* GetName() override;
-	static IShapeFactoryFunctor* CreateFactoryFunctor(std::vector<BaseShape*> shapes);
+	inline virtual const wchar_t* GetName() const override
+	{
+		return this->name.c_str();
+	}
+	virtual void SetPoints(int x1, int y1, int x2, int y2) override;
+	inline static IShapeFactoryFunctor* CreateFactoryFunctor(std::wstring name, std::vector<const BaseShape*> shapes)
+	{
+		return new ComplexShapeFactoryFunctor(name, shapes);
+	}
 	
 public:
 	static constexpr const wchar_t* NAME = L"Complex Shape";
 
 private:
-	std::vector<BaseShape*> shapes;
+	const std::wstring& name;
+	const std::vector<const BaseShape*>& shapes;
+	std::vector<BaseShape*> gfxShapes;
+	int refWidth;
+	int refHeight;
 };
 
